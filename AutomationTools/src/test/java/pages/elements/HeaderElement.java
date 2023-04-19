915d1b6286d3dfde56ad74f6f24ffc1470f1f36e
admin <kms09114@gmail.com>
2023-04-20 00:13:22 +0300

    added getSearchField() method in HeaderElement class

--- AutomationTools/src/test/java/pages/elements/HeaderElement.java
+++ AutomationTools/src/test/java/pages/elements/HeaderElement.java
@@ -2,6 +2,8 @@ package pages.elements;
 
 import com.codeborne.selenide.SelenideElement;
 
+import java.beans.PropertyEditor;
+
 import static com.codeborne.selenide.Selenide.$x;
 
 public class HeaderElement {
@@ -9,5 +11,9 @@ public class HeaderElement {
     private final SelenideElement searchField = $x("//search");
     private final SelenideElement cartCounter = $x("//span[contains(@class, 'badge--green')]");
     private final SelenideElement cartButton = $x("//button[contains(@class, 'header__button--active')]");
+
+    public SelenideElement getSearchField() {
+        return searchField;
+    }
 
 }
