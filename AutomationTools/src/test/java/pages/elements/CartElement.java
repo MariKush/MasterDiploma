e82519d6d32a448dcb8ddadb55f862fff60809ee
admin <kms09114@gmail.com>
2023-04-23 13:29:20 +0300

    add Selenide.sleep(500) in getSumPrice method

--- AutomationTools/src/test/java/pages/elements/CartElement.java
+++ AutomationTools/src/test/java/pages/elements/CartElement.java
@@ -10,7 +10,6 @@ public class CartElement {
 
     private final SelenideElement goodTitle = $x("//a[@class='cart-product__title']");
     private final SelenideElement goodPrice = $x("//p[contains(@class,'cart-product__price')]");
-    private final SelenideElement goodsCount = $x("//input[@data-testid='cart-counter-input']");
     private final SelenideElement sumPrice = $x("//div[@class='cart-receipt__sum-price']/span[1]");
     private final SelenideElement addOneMoreGood = $x("//button[contains(@class, 'cart-counter__button')][2]");
     private final SelenideElement subtractOneGood = $x("//button[contains(@class, 'cart-counter__button')][1]");
@@ -28,6 +27,7 @@ public class CartElement {
     }
 
     public String getSumPrice() {
+        Selenide.sleep(500);
         return sumPrice.shouldBe(visible).getText();
     }
 
